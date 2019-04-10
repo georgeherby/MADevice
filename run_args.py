986aeeb8e309d758f52a9a6ee7825c5588b419bc
George Herbert <georgeherby@hotmail.com>
2019-04-10 11:51:14 +0100

    Fix config type issues

--- run_args.py
+++ run_args.py
@@ -4,13 +4,13 @@ import configargparse
 def get_args():
     parser = configargparse.ArgParser(default_config_files=["config.ini"])
 
-    parser.add_argument('-dt', '--discord_token', required=True,
+    parser.add_argument('-dt', '--discord_token', required=True, type=str,
                         help='Enter token for discord')
 
-    parser.add_argument('-art', '--alert_recheck_time', required=False, default=20,
+    parser.add_argument('-art', '--alert_recheck_time', required=False, default=20, type=int,
                         help='Enter time you want to wait before rechecking status (Default: 20)')
 
-    parser.add_argument('-v', '--verbose', required=False, default=False,
+    parser.add_argument('-v', '--verbose', required=False, default=False, type=bool,
                         help='Verbose logging')
 
     return parser.parse_args()
