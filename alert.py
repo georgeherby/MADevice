89137163500c30594f8719f941584bfeb5155682
George Herbert <georgeherby@hotmail.com>
2019-04-13 10:22:20 +0100

    Fixes & Features (#3)
    * Revert datetime timezone fix
    
    * BREAKING CHANGE: Allow for `alert_recheck_time` and `delay_between_checks` to replace just having one timer of `alert_recheck_time`.
    
    The config variables have changed
    
    * Variable tidy up
    
    * Ensure verbose/debug works

--- alert.py
+++ alert.py
@@ -1,11 +1,10 @@
 import json
 import logging
 import time
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 
 import requests
 from dateutil.parser import parse
-from dateutil.tz import tz
 
 from run_args import get_args
 
@@ -15,12 +14,13 @@ log = logging.getLogger('__name__')
 
 def alert_thread():
     try:
-        alert_time = args.alert_recheck_time
-        discord_data = {
+        duration_before_alert = args.duration_before_alert
+        delay_between_checks = args.delay_between_checks
+        discord_post_data = {
             "username": "MAD Alert",
             "avatar_url": "https://www.iconsdb.com/icons/preview/red/exclamation-xxl.png",
             "embeds": [{
-                "title": f"ERROR - No data for {alert_time} minutes!",
+                "title": f"ERROR - No data for {duration_before_alert} minutes!",
                 "color": 16711680,
                 "description": "PLACEHOLDER",
                 "footer": {
@@ -30,46 +30,47 @@ def alert_thread():
         }
         log.info("Starting Device Checker Script")
         while True:
-            with open('servers.json') as json_file:
-                all_servers_config = json.load(json_file)
-                for server in all_servers_config:
+            with open('servers.json') as servers_json:
+                servers_config_json = json.load(servers_json)
+                for server in servers_config_json:
                     description_initial = f"__**{server['name']}**__\n"
                     description = description_initial
-                    discord_data['embeds'][0]['description'] = description_initial
+                    discord_post_data['embeds'][0]['description'] = description_initial
                     log.info(f"Starting check on {server['ip']}")
                     r = requests.get(f'http://{server["ip"]}/get_status').json()
 
                     for device in r:
-                        log.info(f"Doing check for {device['origin']}")
+                        device_origin = str(device['origin']).title()
+                        device_last_proto_datetime = device['lastProtoDateTime']
+
+                        log.info(f"Checking {device_origin} device")
                         log.debug(device)
-                        origin = str(device['origin'])
-                        if len(device['lastProtoDateTime']) > 0:
+                        if len(device_last_proto_datetime) > 0:
+                            parsed_device_last_proto_datetime = parse(device_last_proto_datetime)
+                            latest_acceptable_datetime = (datetime.now() - timedelta(minutes=duration_before_alert))
+                            log.debug(f"{device_origin} Last Proto Date Time: {parsed_device_last_proto_datetime}")
+                            log.debug(f"{device_origin} Last Acceptable Time: {latest_acceptable_datetime}")
 
-                            last_proto_date_time = parse(device['lastProtoDateTime']).replace(tzinfo=timezone.utc).astimezone(tz=None)
-                            latest_acceptable_time = (datetime.now() - timedelta(minutes=alert_time)).astimezone(tz=tz.tzlocal())
-                            log.debug(f"Last Proto Date Time: {last_proto_date_time}")
-                            log.debug(f"Last Acceptable Time: {latest_acceptable_time}")
-
-                            if last_proto_date_time < latest_acceptable_time:
-                                log.info(f"{origin} breached the time threshold")
-                                description = description + f"{origin.capitalize()} (Last Received: {last_proto_date_time.strftime('%H:%M')})\n"
+                            if parsed_device_last_proto_datetime < latest_acceptable_datetime:
+                                log.info(f"{device_origin} breached the time threshold")
+                                description = description + f"{device_origin.capitalize()} (Last Received: {parsed_device_last_proto_datetime.strftime('%H:%M')})\n"
                                 log.debug(f"Current description: {description}")
                             else:
-                                log.debug(f"{origin} did not breach the time threshold")
+                                log.info(f"{device_origin} did not breach the time threshold")
                         else:
-                            description = description + f"{origin.capitalize()} (Last Received: Not known)\n"
+                            description = description + f"{device_origin.capitalize()} (Last Received: Not known)\n"
 
                     if len(description) > len(description_initial):
-                        discord_data['embeds'][0]['description'] = description
+                        discord_post_data['embeds'][0]['description'] = description
 
-                        next_check_time = (datetime.now() + timedelta(minutes=args.alert_recheck_time)).strftime('%H:%M')
+                        time_of_next_check = (datetime.now() + timedelta(minutes=delay_between_checks)).strftime('%H:%M')
 
-                        discord_data['embeds'][0]['footer']['text'] = f"Next check will be at {next_check_time}"
+                        discord_post_data['embeds'][0]['footer']['text'] = f"Next check will be at {time_of_next_check}"
 
-                        log.debug(discord_data)
-                        log.info("Sending alert for device check issue")
+                        log.debug(discord_post_data)
+                        log.info("Sending alert to Discord as one or more devices has exceeded the threshold")
                         response = requests.post(
-                            server['webhook'], data=json.dumps(discord_data),
+                            server['webhook'], data=json.dumps(discord_post_data),
                             headers={'Content-Type': 'application/json'}
                         )
                         log.debug(response)
@@ -84,6 +85,6 @@ def alert_thread():
                         log.debug("There is no errors to report, going to sleep..")
 
             log.info("All device checks completed, going to sleep!")
-            time.sleep(60 * alert_time)
+            time.sleep(60 * delay_between_checks)
     except Exception as ex:
         log.error('Issues in the checker tread exception was: ' + str(ex))
--- run_args.py
+++ run_args.py
@@ -7,10 +7,18 @@ def get_args():
     parser.add_argument('-dt', '--discord_token', required=True, type=str,
                         help='Enter token for discord')
 
-    parser.add_argument('-art', '--alert_recheck_time', required=False, default=20, type=int,
+    parser.add_argument('-dba', '--duration_before_alert', required=False, default=20, type=int,
+                        help='Enter the number of minutes a device can go before an alert is triggered  (Default: 20)')
+
+    parser.add_argument('-dbc', '--delay_between_checks', required=False, default=20, type=int,
                         help='Enter time you want to wait before rechecking status (Default: 20)')
 
-    parser.add_argument('-v', '--verbose', required=False, default=False, type=bool,
-                        help='Verbose logging')
+    verbose = parser.add_mutually_exclusive_group()
+    verbose.add_argument('-v',
+                         help='Show debug messages',
+                         action='count', default=0, dest='verbose')
+    verbose.add_argument('--verbosity',
+                         help='Show debug messages',
+                         type=int, dest='verbose')
 
     return parser.parse_args()
