fb7f0560ac88cb0bcb994a816939bd2000f75922
Anton Mu <34460584+muckelba@users.noreply.github.com>
2019-06-19 14:22:50 +0200

    Add HTTPS support (#8)

--- connector.py
+++ connector.py
@@ -1,6 +1,7 @@
 import logging
 
 import requests
+requests.packages.urllib3.disable_warnings() 
 
 log = logging.getLogger('__name__')
 
@@ -8,13 +9,21 @@ log = logging.getLogger('__name__')
 def get_status(server):
     try:
         if 'username' in server and 'password' in server:
-            log.debug("MADmin auth used")
-            return requests.get(f'http://{server["ip"]}/get_status', auth=(server['username'], server['password'])).json()
+            if server["ip"].startswith("https://"):
+                log.debug("MADmin auth + HTTPS used")
+                return requests.get(f'{server["ip"]}/get_status', auth=(server['username'], server['password']), verify=False).json()
+            else:
+                log.debug("MADmin auth + HTTP used")
+                return requests.get(f'http://{server["ip"]}/get_status', auth=(server['username'], server['password'])).json()
         else:
             if 'username' in server or 'password' in server:
                 log.warning("Ensure both username and password are set to use authenticated MADmin")
-            log.debug("No MADmin auth used")
-            return requests.get(f'http://{server["ip"]}/get_status').json()
+            if server["ip"].startswith("https://"):
+                log.debug("No MADmin auth + HTTPS used")
+                return requests.get(f'{server["ip"]}/get_status', verify=False).json()
+            else:
+                log.debug("No MADmin auth used")
+                return requests.get(f'http://{server["ip"]}/get_status').json()
     except requests.exceptions.Timeout:
         log.error("Connection to get_status timed-out")
     except requests.exceptions.RequestException as e:
