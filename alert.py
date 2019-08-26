ed32ff86e122b30222f21d1a7ebe997d0fead9bd
George Herbert <georgeherby@hotmail.com>
2019-08-26 12:50:06 +0100

    Add tech debt comment awaiting my PR on MAD

--- alert.py
+++ alert.py
@@ -49,6 +49,7 @@ def alert_thread():
                         log.info(f"Checking {device_origin} device")
                         log.debug(device)
                         if routemanager.lower() != 'idle':
+                            # TODO Remove the 'None' check once MAD has the chagne to remove 'None' from /get_status
                             if len(device_last_proto_datetime) > 0 and device_last_proto_datetime is not None and device_last_proto_datetime != 'None':
                                 parsed_device_last_proto_datetime = parse(device_last_proto_datetime)
                                 latest_acceptable_datetime = (datetime.now() - timedelta(minutes=duration_before_alert))
